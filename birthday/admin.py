236135a330982c551c172a0412904ca5899559e3
Paul Ndambo <paulkadabo@gmail.com>
2021-10-15 20:33:36 +0300

    ci/cd setup

--- birthday/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from . models import Birthday
-# Register your models here.
-admin.site.register(Birthday)
--- birthday/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BirthdayConfig(AppConfig):
-    name = 'birthday'
